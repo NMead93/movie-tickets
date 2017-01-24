710cf67b968e6f89d90f4d1a705ee5a49d72d26d
grinil khanna and nicholas mead <nicholas@>
2017-01-24 09:44:09 -0800

    Initialize site, produce some back end info, building HTML for initial movie presentation to test title click function.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var 5thEl = { name: "5thEl"};
+var GoodWill = { name: "Good Will Hunting"};
+var Sev = { name: "Se7en"};
+var Batman = { name: "The Dark Knight"};
+var zoo = { name: "Zootopia"}
+
+5thEl.mat = { time: [1220], };
+GoodWill.mat = { time: [1250]};
+Sev.mat = { time: [100]};
+Batman.mat = { time: [1220]};
+zoo.mat = { time: [1015, 1145, 159]}
+
+5thEl.ngt = { time: [220, 440, 700, 915]};
+GoodWill.ngt = { time: [330, 600, 1015]};
+Sev.ngt = { time: [300, 520, 735, 1035]};
+Batman.ngt = { time: [310, 630, 850, 1120]};
+zoo.ngt = { time: [330, 545, 730, 945]}
+
+var adult = { mat: "$10.00", ngt: "$12.00"}
+var senior = { mat: "$6.00", ngt: "$9.00"}
+var child = { mat: "$7.00", ngt: "$10.00"}
+var student = { mat: "8.00", ngt: "$10.00"}
+
+function priceCheck ()
