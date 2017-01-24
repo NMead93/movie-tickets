2958f87d384ce7773310be0690bd45ce6c4e456f
grinil khanna and nicholas mead <nicholas@>
2017-01-24 09:56:40 -0800

    add functional 5th ele movie object referencing times (backend)

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,37 @@
-var 5thEl = { name: "5thEl"};
-var GoodWill = { name: "Good Will Hunting"};
-var Sev = { name: "Se7en"};
-var Batman = { name: "The Dark Knight"};
+var fele = { name: "5thEl"};
+var goodWill = { name: "Good Will Hunting"};
+var sev = { name: "Se7en"};
+var batman = { name: "The Dark Knight"};
 var zoo = { name: "Zootopia"}
 
-5thEl.mat = { time: [1220], };
-GoodWill.mat = { time: [1250]};
-Sev.mat = { time: [100]};
-Batman.mat = { time: [1220]};
+var movie =
+
+fele.mat = { time: [1220], };
+goodWill.mat = { time: [1250]};
+sev.mat = { time: [100]};
+batman.mat = { time: [1220]};
 zoo.mat = { time: [1015, 1145, 159]}
 
-5thEl.ngt = { time: [220, 440, 700, 915]};
-GoodWill.ngt = { time: [330, 600, 1015]};
-Sev.ngt = { time: [300, 520, 735, 1035]};
-Batman.ngt = { time: [310, 630, 850, 1120]};
+fele.ngt = { time: [220, 440, 700, 915]};
+goodWill.ngt = { time: [330, 600, 1015]};
+sev.ngt = { time: [300, 520, 735, 1035]};
+batman.ngt = { time: [310, 630, 850, 1120]};
 zoo.ngt = { time: [330, 545, 730, 945]}
 
-var adult = { mat: "$10.00", ngt: "$12.00"}
-var senior = { mat: "$6.00", ngt: "$9.00"}
 var child = { mat: "$7.00", ngt: "$10.00"}
 var student = { mat: "8.00", ngt: "$10.00"}
+var adult = { mat: "$10.00", ngt: "$12.00"}
+var senior = { mat: "$6.00", ngt: "$9.00"}
 
-function priceCheck ()
+function matPrice (custType, period) {
+
+}
+
+$(document).ready(function() {
+
+  $("#5thEl").click(function() {
+    $("#5thEl Ul").append()
+  })
+
+
+})
